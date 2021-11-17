f4bd192092650ec36780a10a434e735adc4989ae
Antony Jones <antony@beyonk.com>
2021-11-17 15:04:56 +0000

    fix wrangler config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,11 @@
+import adapter from '@sveltejs/adapter-cloudflare-workers'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		adapter: adapter()
 	}
 };
 
